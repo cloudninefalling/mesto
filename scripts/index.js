e1e7c7704cb86d73e07205c918a4ce81cd7d9a09
cloudninefalling <shalygin.da7@gmail.com>
2023-05-08 20:06:34 +0300

    fix: wrong margins fixed, re-wrapped profile-edit form, fixed profile overflow

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,41 @@
 let page = document.querySelector('.page');
+
+//query selectors for profile elements
 let profile = page.querySelector('.profile');
 let profileInfo = profile.querySelector('.profile-info');
 let userName = profileInfo.querySelector('.profile-info__name');
 let userOccupation = profileInfo.querySelector('.profile-info__occupation');
 let editButton = profileInfo.querySelector('.profile-info__edit-button');
 
+//query selectors for popup elements
 let popup = page.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close');
-let submitButton = popup.querySelector('.popup__submit');
-let popupForm = popup.querySelector('.popup__container');
-let inputName = popupForm.querySelector('.popup__name');
-let inputOccupation = popupForm.querySelector('.popup__occupation');
+let popupContainer = popup.querySelector('.popup__container');
 
+//query selectors for form elements
+let form = popupContainer.querySelector('.edit-form');
+let inputName = form.querySelector('.edit-form__text_input_name');
+let inputOccupation = form.querySelector('.edit-form__text_input_occupation');
+
+//open popup, set input values
 function openPopup() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
   inputName.value = userName.textContent;
   inputOccupation.value = userOccupation.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup__opened');
+  popup.classList.remove('popup_opened');
 }
 
+//close popup, update profile info
 function submitPopup(evt) {
   evt.preventDefault();
   userName.textContent = inputName.value;
   userOccupation.textContent = inputOccupation.value;
-  popup.classList.remove('popup__opened');
+  closePopup();
 }
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
-submitButton.addEventListener('click', submitPopup);
+form.addEventListener('submit', submitPopup);
